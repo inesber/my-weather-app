643ae57630c3239c6a0c6c99455dfdf8b6dd8ae7
inesber <55463738+inesber@users.noreply.github.com>
2020-04-12 15:05:39 +0100

    Added Default City

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -88,8 +88,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "0c7738f69f93f7a10985af2c2d89420a";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
